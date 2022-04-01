6bfd94c31722a09be187ea111d912c7bf02dc4ea
kyleraykbs <44762123+kyleraykbs@users.noreply.github.com>
2022-03-31 19:49:53 -0700

    Adde Linked Partner Code

--- lua/entities/prop_portal/init.lua
+++ lua/entities/prop_portal/init.lua
@@ -11,6 +11,32 @@ local function CorrectPortalAng(ang)
     return ang
 end
 
+local function FindAndSetLinkedPartner(prop_portal)
+    if (!prop_portal.LinkedPartner or !prop_portal.LinkedPartner:IsValid() or !prop_portal.LinkedPartner.IsActive or !prop_portal.LinkedPartner.Portal:IsValid()) then
+        print("No previously linked partner found... finding one now")
+
+        -- go through every portal in the map 
+        for _, portal in pairs(ents.FindByClass("prop_portal")) do
+            if (portal ~= prop_portal) then
+                -- if the portal is linked to the current portal
+                if ( portal.LinkedPartner == nil and portal.IsActive) then
+                    print("Found linked partner!")
+
+                    -- set the current portal's linked partner to the portal we found
+                    prop_portal.LinkedPartner = portal
+
+                    -- set the portal we found's linked partner to the current portal
+                    portal.LinkedPartner = prop_portal
+
+                    return portal
+                end
+            end
+        end
+    else
+        return prop_portal.LinkedPartner
+    end
+end
+
 local function SetPortalPos(portal, pos, ang)
     portal:SetPos(pos)
     portal:SetAngles(ang)
@@ -24,14 +50,42 @@ local function SpawnPortal()
     return Portal
 end
 
+local function UpdatePortalLinkState(Portal)
+    FindAndSetLinkedPartner(Portal)
+    if Portal.LinkedPartner ~= nil then
+
+        -- link the portal
+        Portal.Portal:LinkPortal(Portal.LinkedPartner.Portal)
+
+    end
+end
+
+
 local function RemovePortal(Portal)
-    Portal:Remove()
+    Portal.IsActive = false
+    if !Portal.Portal then
+        print("Portal not found")
+        return
+    else
+        if Portal.Portal and Portal.Portal:IsValid() then
+            Portal.Portal:Remove()
+        end
+    end
+    local PreviousPartner = Portal.LinkedPartner
+    Portal.LinkedPartner = nil
+    if PreviousPartner ~= nil and PreviousPartner:IsValid() then
+        PreviousPartner.LinkedPartner = nil
+        UpdatePortalLinkState(PreviousPartner)
+    end
 end
 
 function ENT:Initialize( )
     print( "___Portal Initialize___" )
     print( self:GetPos() )
     print( self:GetAngles() )
+    self.IsActive = false
+    self.LinkedPartner = nil
+    self.Portal = nil
     for k, v in pairs(self:GetKeyValues()) do
         print(k, v)
     end
@@ -46,7 +100,8 @@ function ENT:AcceptInput( inputname, activator, caller, data )
     print( data )
     print( "________________________" )
 
-    -- SET ACTIVEATED STATE ----------------------
+    -- SET ACTIVATED STATE ----------------------
+
     if (inputname == "SetActivatedState") then
         -- create portal
         if (data == "1") then
@@ -54,27 +109,38 @@ function ENT:AcceptInput( inputname, activator, caller, data )
             if !self.Portal or !self.Portal:IsValid() then
                 self.Portal = SpawnPortal()
             end
-
+            
             SetPortalPos(self.Portal, self:GetPos(), CorrectPortalAng(self:GetAngles()))
+            
+            print ("__ Linking Portal __")
+            self.IsActive = true
+            FindAndSetLinkedPartner(self)
+
+            print(self)
+            print(self.Portal)
+
+            if self.LinkedPartner ~= nil then
+
+                -- link the portal
+                self.Portal:LinkPortal(self.LinkedPartner.Portal)
+
+            end
         end
 
         -- remove portal
         if (data == "0") then
             -- Remove A Actual Portal
-            if self.Portal or !self.Portal:IsValid() then
-                RemovePortal(self.Portal)
-            end
+            RemovePortal(self)
         end
     end
+
     ------------------------------------------------
 
     -- FIZZLE --------------------------------------
 
     if (inputname == "Fizzle") then
         -- Remove A Actual Portal
-        if self.Portal or !self.Portal:IsValid() then
-            RemovePortal(self.Portal)
-        end
+        RemovePortal(self)
     end
 
     ------------------------------------------------
