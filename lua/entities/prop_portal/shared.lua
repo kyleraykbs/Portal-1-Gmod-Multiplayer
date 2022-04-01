b8ef5fa9abb68d571b062759903f3ec4ff292c33
kyleraykbs <44762123+kyleraykbs@users.noreply.github.com>
2022-03-31 17:36:38 -0700

    added portal ent start

---
+++ lua/entities/prop_portal/shared.lua
@@ -0,0 +1,14 @@
+print("Hello Portal! (im a portal shared.lua file)")
+
+ENT.Type = "anim";
+
+ENT.PrintName = "Portal";
+ENT.Author = "kyleraykbs / wolfe strider shooter";
+ENT.Contact = "";
+ENT.Purpose = "Speedy thing goes in speedy thing goes out";
+ENT.Instructions = "fuck off im a portal";
+
+ENT.RenderGroup = RENDERGROUP_BOTH
+
+ENT.Spawnable			= false
+ENT.AdminSpawnable		= false
